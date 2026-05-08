8c4c673e3ecfcdeb123b662303dd66ea1a30cdad
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2026-05-08 18:27:51 +0000

    fix: use HTMLDialogElement type for DialogContentProps

--- packages/dashboard/src/components/ui/dialog.tsx
+++ packages/dashboard/src/components/ui/dialog.tsx
@@ -55,7 +55,7 @@ function DialogTrigger({ asChild, children }: DialogTriggerProps) {
   )
 }
 
-interface DialogContentProps extends React.HTMLAttributes<HTMLDivElement> {
+interface DialogContentProps extends React.HTMLAttributes<HTMLDialogElement> {
   children: React.ReactNode
 }
 
